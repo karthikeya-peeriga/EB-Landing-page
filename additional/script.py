6055065ae639908a13a06958d84f4c502364b805
Peeriga Karthikeya <153732471+karthikeya-peeriga@users.noreply.github.com>
2024-12-05 19:17:49 +0530

    Removed Video section

--- additional/script.py
+++ additional/script.py
@@ -2,7 +2,7 @@ from PIL import Image
 import os
 
 # Specify the directory containing the images for the dictionary
-image_directory = "../resized"
+image_directory = "../images"
 
 # Dictionary to store image names and their dimensions
 image_dimensions = {}
